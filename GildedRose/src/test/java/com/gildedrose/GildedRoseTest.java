bbe3ff87a31fed0b987fd255ee40cdee9c70b92f
Antoine JOUET <ajouet@bouyguestelecom.fr>
2021-05-19 13:13:27 +0200

    ajout approvals maven

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,12 +3,12 @@ package com.gildedrose;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class GildedRoseTest {
+class GildedRoseTest {
 
     private static final String SULFURAS =  "Sulfuras, Hand of Ragnaros";
 
     @Test
-    public void should_decrease_item_quality(){
+    void should_decrease_item_quality(){
         Item regularItem = new Item("Regular Item", 3, 10);
 
         GildedRose gildedRose = new GildedRose(new Item[] { regularItem } );
@@ -18,7 +18,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void should_decrease_twice_item_quality(){
+    void should_decrease_twice_item_quality(){
         Item regularItem = new Item("Regular Item", 1, 10);
         GildedRose gildedRose = new GildedRose(new Item[] { regularItem } );
 
@@ -29,7 +29,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void should_decrease_to_zero_item_quality(){
+    void should_decrease_to_zero_item_quality(){
         Item regularItem = new Item("Regular Item", 1, 1);
         GildedRose gildedRose = new GildedRose(new Item[] { regularItem } );
 
@@ -40,7 +40,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void should_decrease_sulfuras_item_quality(){
+    void should_decrease_sulfuras_item_quality(){
         Item regularItem = new Item(SULFURAS, 1, 10);
         GildedRose gildedRose = new GildedRose(new Item[] { regularItem } );
 
