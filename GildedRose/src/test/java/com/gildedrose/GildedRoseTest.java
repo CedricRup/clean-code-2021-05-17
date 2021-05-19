e75a30c8da0d83e2c6d84e97fba4aee013969a58
Vincent MEUNIER <vimeunie@bouyguestelecom.fr>
2021-05-19 14:06:32 +0200

    Mutation testing GildedRose

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,63 +1,31 @@
 package com.gildedrose;
 
 import org.approvaltests.combinations.CombinationApprovals;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class GildedRoseTest {
 
-    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+	private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+	private static final String AGED_BRIE = "Aged Brie";
+	private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
 
-    @Test
-    void should_decrease_item_quality() {
-        // Given
-        String[] names = new String[]{ "Regular Item", SULFURAS };
-        Integer[] sellIns = new Integer[]{ 3, 0 };
-        Integer[] qualities = new Integer[]{ 10, 0 };
+	@Test
+	void should_decrease_item_quality() {
+		// Given
+		final String[] names = new String[] { "Regular Item", SULFURAS, AGED_BRIE, BACKSTAGE };
+		final Integer[] sellIns = new Integer[] { -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+		final Integer[] qualities = new Integer[] { 0, 10, 48, 49, 50, 51, 52, 61, 1000 };
 
-        // When / Then
-        CombinationApprovals.verifyAllCombinations(this::getItem, names, sellIns, qualities);
-    }
+		// When / Then
+		CombinationApprovals.verifyAllCombinations(this::getItem, names, sellIns, qualities);
+	}
 
-    private Item getItem(String name, int sellIn, int quality) {
-        Item regularItem = new Item(name, sellIn, quality);
+	private Item getItem(String name, int sellIn, int quality) {
+		final Item regularItem = new Item(name, sellIn, quality);
 
-        GildedRose gildedRose = new GildedRose(new Item[]{ regularItem });
-        gildedRose.updateQuality();
-        return regularItem;
-    }
-
-    @Test
-    void should_decrease_twice_item_quality() {
-        Item regularItem = new Item("Regular Item", 1, 10);
-        GildedRose gildedRose = new GildedRose(new Item[]{ regularItem });
-
-        gildedRose.updateQuality();
-        gildedRose.updateQuality();
-
-        Assertions.assertEquals(7, regularItem.quality);
-    }
-
-    @Test
-    void should_decrease_to_zero_item_quality() {
-        Item regularItem = new Item("Regular Item", 1, 1);
-        GildedRose gildedRose = new GildedRose(new Item[]{ regularItem });
-
-        gildedRose.updateQuality();
-        gildedRose.updateQuality();
-
-        Assertions.assertEquals(0, regularItem.quality);
-    }
-
-    @Test
-    void should_decrease_sulfuras_item_quality() {
-        Item regularItem = new Item(SULFURAS, 1, 10);
-        GildedRose gildedRose = new GildedRose(new Item[]{ regularItem });
-
-        gildedRose.updateQuality();
-        gildedRose.updateQuality();
-
-        Assertions.assertEquals(10, regularItem.quality);
-    }
+		final GildedRose gildedRose = new GildedRose(new Item[] { regularItem });
+		gildedRose.updateQuality();
+		return regularItem;
+	}
 
 }
