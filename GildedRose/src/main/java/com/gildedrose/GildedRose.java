6a9364c9eaf150f0b1cd078647be64e60571cd6f
Romain PIFFETEAU <RPIFFETE@bouyguestelecom.fr>
2021-05-19 14:52:54 +0200

    Refacto

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -2,60 +2,60 @@ package com.gildedrose;
 
 class GildedRose {
 
-    private static final String AGED_BRIE = "Aged Brie";
-    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
-    private static final String SULFURAS  = "Sulfuras, Hand of Ragnaros";
+	private static final int MAX_QUALITY = 50;
+	private static final String AGED_BRIE = "Aged Brie";
+	private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+	private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
-    Item[] items;
+	Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+	public GildedRose(Item[] items) {
+		this.items = items;
+	}
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            Item item = items[i];
+	public void updateQuality() {
+		for (int i = 0; i < items.length; i++) {
+			final Item item = items[i];
 
-            if (item.name.equals(SULFURAS)) {
+			if (item.name.equals(SULFURAS)) {
+			} else {
 
-            }
-            else {
+				if (item.name.equals(AGED_BRIE) || item.name.equals(BACKSTAGE)) {
+					if (isItemQualityUnderMax(item)) {
+						item.quality++;
 
-                if (item.name.equals(AGED_BRIE) || item.name.equals(BACKSTAGE)) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+						if (item.name.equals(BACKSTAGE)) {
+							if (item.sellIn < 11 && isItemQualityUnderMax(item)) {
+								item.quality++;
+							}
 
-                        if (item.name.equals(BACKSTAGE)) {
-                            if (item.sellIn < 11 && item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+							if (item.sellIn < 6 && isItemQualityUnderMax(item)) {
+								item.quality++;
+							}
+						}
+					}
+				} else if (item.quality > 0) {
+					item.quality--;
+				}
 
-                            if (item.sellIn < 6 && item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-                else if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+				item.sellIn--;
 
-                item.sellIn = item.sellIn - 1;
+				if (item.sellIn < 0) {
+					if (item.name.equals(AGED_BRIE)) {
+						if (isItemQualityUnderMax(item)) {
+							item.quality++;
+						}
+					} else if (item.name.equals(BACKSTAGE)) {
+						item.quality = 0;
+					} else if (item.quality > 0) {
+						item.quality--;
+					}
+				}
+			}
+		}
+	}
 
-                if (item.sellIn < 0) {
-                    if (item.name.equals(AGED_BRIE)) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                    else if (item.name.equals(BACKSTAGE)) {
-                        item.quality = item.quality - item.quality;
-                    }
-                    else if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            }
-        }
-    }
+	private boolean isItemQualityUnderMax(final Item item) {
+		return item.quality < MAX_QUALITY;
+	}
 }
